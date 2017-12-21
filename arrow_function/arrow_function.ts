69ea5159439ef97fbd7af98a405b55d7552f8881
ratikanta-sdrc <ratikanta@sdrc.co.in>
2017-12-21 17:51:45 +0530

    variable declaration added

---
+++ arrow_function/arrow_function.ts
@@ -0,0 +1,66 @@
+// var getRegularValue = function (){
+//     return 10
+// }
+
+// console.log(getRegularValue())
+
+// const getArrowValue = () => {
+//     return 10
+// }
+
+// console.log(getArrowValue())
+
+// const getArrowValue = () => 10
+
+
+// console.log(getArrowValue())
+
+
+
+// const getArrowValue = (m) => 10 * m
+
+
+// console.log(getArrowValue(3))
+
+
+// const getArrowValue = m => 10 * m
+
+
+// console.log(getArrowValue(3))
+
+
+
+// const getArrowValue = (m, bonus) => 10 * m + bonus
+
+
+// console.log(getArrowValue(3, 10))
+
+// console.log(typeof(getArrowValue))
+
+
+
+// const getArrowValue = (m, bonus) => {
+
+//     if(m> 2)
+//         bonus *= 2
+
+//     return 10 * m + bonus
+
+// }
+
+
+// console.log(getArrowValue(3, 10))
+
+
+//This keyword problem
+
+var employee = {
+    id: 1,
+    greet: function(){
+        setTimeout(()=>{
+            console.log(this.id)
+        }, 1000)        
+    }
+}
+
+employee.greet()
