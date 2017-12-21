2300d04865a384a9ba59664b52398e46dfc28f9a
ratikanta-sdrc <ratikanta@sdrc.co.in>
2017-12-21 16:50:41 +0530

    variable declaration added

---
+++ variable-declaration/hello.ts
@@ -0,0 +1,42 @@
+
+//One limitation
+// function f(shouldInitialize: boolean) {
+//     if (shouldInitialize) {
+//         var x = 10;
+//     }
+
+//     return x;//dangerous
+// }
+// console.log(f(true));  // returns '10'
+// console.log(f(false));
+
+
+// function f(shouldInitialize: boolean) {
+//     if (shouldInitialize) {
+//         let x = 10;
+//     }
+
+//     return x;//dangerous
+// }
+
+// console.log(f(true));  // returns '10'
+// console.log(f(false));
+
+//not showing error declare variable multiple times
+// function sumMatrix() {
+//     var sum = 0;
+//     var row = 5
+//     var column = 10
+//     for (var i = 0; i < row; i++) {        
+//         for (var i = 0; i < column; i++) {
+//             sum++;
+//         }
+//     }
+//     return sum;
+// }
+
+// console.log(sumMatrix())//expected 50, will get 5
+
+for (var i = 0; i < 10; i++) {
+    setTimeout(function() { console.log(i); }, 100 * i);
+}
