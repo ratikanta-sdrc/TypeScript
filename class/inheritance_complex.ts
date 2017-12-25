3350f2aa695864fbe378ef2b8e8e02829dbafcf6
ratikanta-sdrc <ratikanta@sdrc.co.in>
2017-12-25 14:07:42 +0530

    inheritance example added

---
+++ class/inheritance_complex.ts
@@ -0,0 +1,29 @@
+class Animal1 {
+    name: string;
+    constructor(theName: string) { this.name = theName; }
+    move(distanceInMeters: number = 0) {
+        console.log(`${this.name} moved ${distanceInMeters}m.`);
+    }
+}
+
+class Snake extends Animal1 {
+    constructor(name: string) { super(name); }
+    move(distanceInMeters = 5) {
+        console.log("Slithering...");
+        super.move(distanceInMeters);
+    }
+}
+
+class Horse extends Animal1 {
+    constructor(name: string) { super(name); }
+    move(distanceInMeters = 45) {
+        console.log("Galloping...");
+        super.move(distanceInMeters);
+    }
+}
+
+let guguli = new Snake("Guguli the Sapa");
+let harsh: Animal1 = new Horse("Harsh the Ghoda");
+
+guguli.move();
+harsh.move(34);
