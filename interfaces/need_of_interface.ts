edd6b88f9b1bee981dd9beab3878af566fd8c342
ratikanta-sdrc <ratikanta@sdrc.co.in>
2018-03-27 22:35:14 +0530

    interface modified

--- interfaces/need_of_interface.ts
+++ interfaces/need_of_interface.ts
@@ -1,9 +1,56 @@
-const showUserDetails = function(name, age, address){
-    console.log("Name of the user: " + name) 
-    console.log("Age of the user: " + age) 
-    console.log("Address of the user: " + address) 
+//step 1
+
+
+// let showUserDetails = (name, age, address) => {
+
+
+//     console.log("Name of the user: " + name) 
+//     console.log("Age of the user: " + age) 
+//     console.log("Address of the user: " + address) 
+
+
+// }
+// showUserDetails("SK Azar", 28, "Bhubaneswar, Odisha, India")
+
+
+
+
+
+//step 2
+//Inline annotation
+
+// let showUserDetails = (person: {name: string, age: number, address: string}) => {
+
+
+//     console.log("Name of the user: " + person.name) 
+//     console.log("Age of the user: " + person.age) 
+//     console.log("Address of the user: " + person.address) 
+
+
+// }
+// showUserDetails({name : "SK Azar", age: 28, address: "Bhubaneswar, Odisha, India"})
+
+
+//Step 3
+//User of interface
+//This is cleaner and can be used at multiple places
+//This is where we need interface
+
+
+interface IPerson{
+    name: string,
+    age: number,
+    address: string
 }
 
-showUserDetails("SK Azar", 28, "Bhubaneswar, Odisha, India")
 
-//Export azar
+let showUserDetails = (person: IPerson) => {
+
+
+    console.log("Name of the user: " + person.name) 
+    console.log("Age of the user: " + person.age) 
+    console.log("Address of the user: " + person.address) 
+
+
+}
+showUserDetails({name : "SK Azar", age: 28, address: "Bhubaneswar, Odisha, India"})
