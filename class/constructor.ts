2d975a2efd3065b15eb8c6c1042556ed9aa80de6
ratikanta-sdrc <ratikanta@sdrc.co.in>
2018-04-02 11:19:03 +0530

    constructor code added

---
+++ class/constructor.ts
@@ -0,0 +1,20 @@
+class Person{
+    name: string
+    age: number
+    address: string
+
+
+    
+    constructor(){
+        console.log('Hello from constructor...')
+    }
+
+}
+
+let malay = new Person()
+malay.name = 'Malay'
+
+console.log(malay.name)
+
+//We will test setting property with help of constructor
+
